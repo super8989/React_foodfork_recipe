b013dd2760190abb73c971ef2d3eae0b307b2fa1
Sam <super8989@gmail.com>
2020-01-07 21:46:26 -0500

    passed props to Header in Home.js to render title and button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 
+import { Link } from "react-router-dom";
+
 class Home extends Component {
 	render() {
-		return <Header />;
+		return (
+			<Header title='amazing recipes'>
+				<Link
+					to='/recipes'
+					className='text-uppercase btn btn-secondary btn-lg mt-3'
+				>
+					search recipes
+				</Link>
+			</Header>
+		);
 	}
 }
 
