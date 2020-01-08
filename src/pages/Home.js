1ee815d90de063193f79bfac7022179c9a7db0ee
Sam <super8989@gmail.com>
2020-01-07 21:34:20 -0500

    created Header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
 
 class Home extends Component {
 	render() {
-		return (
-			<div>
-				<h4> this is homepage home.js</h4>
-			</div>
-		);
+		return <Header />;
 	}
 }
 
