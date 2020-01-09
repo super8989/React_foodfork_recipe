cb9ebbf2bf7690864548497421f9d18a2d46858d
Sam <super8989@gmail.com>
2020-01-08 21:24:18 -0500

    added input form and button to Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,42 @@ import React, { Component } from "react";
 
 class Search extends Component {
 	render() {
+		const { handleChange, handleSubmit, search } = this.props;
+
 		return (
-			<div>
-				<h1> hello from search</h1>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-10 mx-auto col-md-8 mt-5 text-center'>
+						<h1 className='text-slanted text-capitalize'>
+							search recipes with
+							<strong className='text-orange'> Food 2 Fork</strong>
+						</h1>
+						<form className='mt-4'>
+							<label htmlFor='search' className='text-capitalize'>
+								type recipes separated by comma
+							</label>
+							<div className='input-group'>
+								<input
+									type='text'
+									name='search'
+									className='form-control'
+									placeholder='chicken, onion, carrots'
+									value={search}
+									onChange={handleChange}
+								/>
+								<div className='input-group-append'>
+									<button
+										type='submit'
+										className='input-group-text bg-primary text-white'
+										onClick={handleSubmit}
+									>
+										<i className='fas fa-search' />
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
 			</div>
 		);
 	}
