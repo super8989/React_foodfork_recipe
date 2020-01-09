7b048abf0936516e1e9f339053a24cfa0fbbadd1
Sam <super8989@gmail.com>
2020-01-08 20:54:12 -0500

    created new components

---
+++ src/components/RecipeList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Recipe from "./Recipe";
+
+class RecipeList extends Component {
+	render() {
+		return (
+			<div>
+				<h1> hello from RecipeList</h1>
+				<Recipe />
+			</div>
+		);
+	}
+}
+
+export default RecipeList;
