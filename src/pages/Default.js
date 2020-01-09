a0bac45d6fcb2017cb4052075a8770009eff8c42
Sam <super8989@gmail.com>
2020-01-08 20:42:34 -0500

    added Header and Link to default page

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 class Default extends Component {
 	render() {
 		return (
-			<div>
-				<h4> this is error page</h4>
-			</div>
+			<Header title='404' styleClass='default-hero'>
+				<h2 className='text-light text-uppercase'>
+					You are in the wrong place
+				</h2>
+				<Link to='/' className='text-uppercase btn btn-secondary btn-lg mt-3'>
+					return home
+				</Link>
+			</Header>
 		);
 	}
 }
