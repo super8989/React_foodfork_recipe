a5d16883f3ea0528060a4e2c516e268716085783
Sam <super8989@gmail.com>
2020-01-08 21:00:20 -0500

    setting up Recipes.js with state from temList

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import { recipeData } from "../data/tempList";
+
 class Recipes extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	state = {
+		recipes: recipeData,
+		search: ""
+	};
+
 	render() {
 		return (
-			<div>
-				<h4> this is recipes page</h4>
-			</div>
+			<>
+				<Search />
+				<RecipeList />
+			</>
 		);
 	}
 }
